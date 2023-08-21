985a7d295776fff03b161fc508955e2bd7ca1ee5
NurulHelda <nurul.helda.tif20@gmail.com>
2023-08-22 01:13:28 +0700

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const randomImageBtn = document.getElementById('get-random-image');
 const randomImage = document.getElementById('random-image');
 
-randomImageBtn.addEventListener('click', async () => {
+async function loadRandomImage() {
   try {
     const response = await fetch('https://source.unsplash.com/500x400/?random');
     randomImage.src = response.url;
   } catch (error) {
     console.error('Error fetching image:', error);
   }
-});
+}
+
+// Memuat gambar secara otomatis saat halaman dimuat
+loadRandomImage();
+
+randomImageBtn.addEventListener('click', loadRandomImage);
